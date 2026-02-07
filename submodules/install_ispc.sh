71e2e39f99cde06541c7ee796cdbf38c41fe2e26
Daniel Kulp <dan@kulp.com>
2026-02-07 13:15:24 -0500

    Update ispc and wxwidgets

--- submodules/install_ispc.sh
+++ submodules/install_ispc.sh
@@ -5,10 +5,9 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 BASE_DEPS_DIR=$( dirname -- "${SCRIPT_DIR}" )
 
-ISPC_VERSION=v1.28.2
+ISPC_VERSION=v1.30.0
 wget https://github.com/ispc/ispc/releases/download/${ISPC_VERSION}/ispc-${ISPC_VERSION}-macOS.universal.tar.gz
 tar -xzf ispc-${ISPC_VERSION}-macOS.universal.tar.gz
 cp ispc-${ISPC_VERSION}-macOS.universal/bin/ispc ${BASE_DEPS_DIR}/bin
 rm -rf ispc-${ISPC_VERSION}-macOS.universal*
-
 
